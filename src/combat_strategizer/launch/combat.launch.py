eb4b6015ee23b2b9b9856eb86ef39f773b9b8826
bjomech <batten_rhodium_0v@icloud.com>
2026-03-26 21:14:02 +0100

    interface adaption

--- src/combat_strategizer/launch/combat.launch.py
+++ src/combat_strategizer/launch/combat.launch.py
@@ -14,60 +14,31 @@ def generate_launch_description():
         executable='simple_navigator',
         name='simple_navigator',
         output='screen',
-        parameters=[
-            os.path.join(
-                get_package_share_directory('robot_navigation'),
-                'config',
-                'params.yaml',
-            ),
-            {'log_level': 'warn'}]
     )
 
     combat_strategizer = Node(
         package='combat_strategizer',
         executable='simple_attack',
         name='simple_attack',
-        parameters=[{'log_level': 'warn'}]  # Set the logging level to 'warn'
     )
 
-    # Include the slider_publisher launch file with the parameters
-    slider_publisher = IncludeLaunchDescription(
-        XMLLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('slider_publisher'),
-                'examples',
-                'example.launch'
-            )
-        ),
-        launch_arguments={'file': './config/Float32.yaml'}.items()
+    opponent_tf_to_pose = Node(
+        package='combat_strategizer',
+        executable='tf_to_pose',
+        name='opponent_tf_to_pose',
+        parameters=[{
+            'source_frame': 'opponent',
+            'reference_frame': 'world',
+            'pose_topic': '/opponent/pose',
+        }],
+        output='screen'
     )
 
-    # Include the slider_publisher launch file with the parameters
-    slider_publisher = IncludeLaunchDescription(
-        XMLLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('robot_bringup'),
-                'launch',
-                'weaponspeed_slider.launch'
-            )
-        ),
-        launch_arguments={'file': 'Float32.yaml'}.items()
-    )
 
-    # Add micro-ROS agent as a Node
-    micro_ros_agent = Node(
-        package='micro_ros_agent',
-        executable='micro_ros_agent',
-        name='micro_ros_agent',
-        output='screen',
-        arguments=['udp4', '--port', '8888', '--dev', '	192.168.1.103']  #192.168.8.210
-    )
 
     # Build launch description
     ld = LaunchDescription()
     ld.add_action(simple_navigator)
     ld.add_action(combat_strategizer)
-    ld.add_action(slider_publisher)
-    ld.add_action(micro_ros_agent) 
-
+    ld.add_action(opponent_tf_to_pose)
     return ld
